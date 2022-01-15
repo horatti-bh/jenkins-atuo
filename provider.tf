609ba6320b60a3f89979a00f935097ffc5427475
CT <ct@gmail.com>
2022-01-15 17:53:15 +0530

    added tf setup files

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region            = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket          = "terraform-states"
-    key             = "jenkins/terraform.tfstate"
-    region          = "us-east-1"
-    dynamodb_table  = "terraform"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket          = "terraform-states"
+#    key             = "jenkins/terraform.tfstate"
+#    region          = "us-east-1"
+#    dynamodb_table  = "terraform"
+#  }
+#}
