fc630a5486ad57cbc292973dd6f2dd76e4a85da7
CT <ct@gmail.com>
2022-01-21 22:29:03 +0530

    Jenkins basics

--- jobs.groovy
+++ jobs.groovy
@@ -3,3 +3,18 @@ folder('Terraform') {
     description('Terraform_jobs')
 }
 
+
+job('VPC') {
+    scm {
+        git {
+            remote {
+                name('GIT')
+                url('https://github.com/horatti-bh/terraform_vpc.git')
+                credentials('GIT')
+            }
+            extensions {
+                cleanAfterCheckout()
+            }
+        }
+    }
+}
